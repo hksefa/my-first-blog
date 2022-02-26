52a286fd8e9b595c6881ba9554fbb0c4bd1b4fed
hksefa <sefahakyemez94@gmail.com>
2022-02-27 00:31:06 +0300

    Django Girls uygulamam, ilk commit

---
+++ ilk_django_projem/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ilk_django_projem project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ilk_django_projem.settings')
+
+application = get_asgi_application()
